bd6e1b6079a35ac4c633be804478651c82550fa4
zerex21 <zerex21@icloud.com>
2020-02-24 22:39:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = n + '';
+    let reversedText = n.split('').reverse().join('');
+    let reversedNumber = parseInt(reversedText, 10);
+    console.log("reversed number: ", reversedNumber);
+    return reversedNumber;
 }
